2fdf0dd43d92e753ea9642e60787e65d9e1f837a
Monika Williams <monika.m.williams@gmail.com>
2018-01-02 13:39:31 +0000

    automatically backed up by learn

--- src/components/SimplerComponent.js
+++ src/components/SimplerComponent.js
@@ -1,6 +1,5 @@
 // Code SimplerComponentHere Here
-class HelloWorld extends Component {
-  render() {
-    return <div> "I am just happy"</div>;
-  }
-}
+import React from 'react';
+
+const SimplerComponent = props => <div onClick={props.handleClick}> I am just happy. </div>;
+export default SimplerComponent;
